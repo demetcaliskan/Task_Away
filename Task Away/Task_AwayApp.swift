00182ab967d08c0ba94f27baeb71eef105c90367
Demet Çalışkan <caliskand@mef.edu.tr>
2023-10-24 23:17:15 +0300

    Initial Commit

---
+++ Task Away/Task_AwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_AwayApp.swift
+//  Task Away
+//
+//  Created by Demet Çalışkan on 24.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Task_AwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
